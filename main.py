12d4ca95bc3543bba41937f492c088c7199b6c56
danila <gonilegy2@gmail.com>
2026-03-15 14:25:15 +0300

    добавил структуру проекта

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def main():
+    print("Проект запущен")
+    print("Скрипты:")
+    print("- currency.py  — курсы валют")
+    print("- posts.py     — посты из API")
+    print("- bot.py       — мини бот")
+    print("- async_api.py — async запросы")
+
+if __name__ == "__main__":
+    main()
